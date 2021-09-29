cc7571896f598bda69de1a85ba3cd9bfbbcd8d3d
Basar Buyukkahraman <basarbk@yahoo.com>
2021-09-29 17:36:45 +0300

    Mock Service Worker introduced

--- src/RandomUser.spec.js
+++ src/RandomUser.spec.js
@@ -1,8 +1,8 @@
 import RandomUser from './RandomUser.svelte';
 import { fireEvent, render, screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom';
-import axios from "axios";
-import "whatwg-fetch";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 
 describe('Random User', () => {
   it('has button to load random user', () => {
@@ -13,26 +13,26 @@ describe('Random User', () => {
     expect(loadButton).toBeInTheDocument();
   });
   it('displays title, first and lastname of loaded user from randomuser.me', async () => {
+    const server = setupServer(
+      rest.get("https://randomuser.me/api", (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({
+          results: [
+            {
+              name: {
+                title: 'Miss',
+                first: 'Jennifer',
+                last: 'Alvarez'
+              }
+            }
+          ]
+        }));
+      })
+    );
+    server.listen();
     render(RandomUser);
     const loadButton = screen.queryByRole('button', {
       name: 'Load Random User'
     });
-
-    const mockApiCall = jest.fn().mockResolvedValue({
-      data: {
-        results: [
-          {
-            name: {
-              title: 'Miss',
-              first: 'Jennifer',
-              last: 'Alvarez'
-            }
-          }
-        ]
-      }
-    });
-
-    axios.get = mockApiCall;
     fireEvent.click(loadButton);
     const userInfo = await screen.findByText("Miss Jennifer Alvarez");
     expect(userInfo).toBeInTheDocument();
